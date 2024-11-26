b0ba6fa76f4cb3d2342fa61378aa8b4389fd16f8
zaph <zaph@mail.com>
2024-11-26 12:46:05 -0600

    added template files

--- openstack_server_setup.sh
+++ openstack_server_setup.sh
@@ -1,3 +1,47 @@
+#!/bin/bash
+    SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+    sudo timedatectl set-timezone America/Mexico_City
+    sudo apt install ntp -y
+    sudo service ntp stop
+    sudo ntpd -gq
+    sudo service ntp start
+
+    sudo apt install git python3-dev libffi-dev python3-venv gcc libssl-dev git python3-pip python3-full
+    python3 -m venv $HOME/kolla-openstack
+    source $HOME/kolla-openstack/bin/activate
+    pip install -U pip
+    pip install 'ansible>=8,<9'
+    mkdir kolla-openstack-ansible
+    cd kolla-openstack-ansible
+    cp $SCRIPT_DIR/ansible.cfg ansible.cfg
+    pip install git+https://opendev.org/openstack/kolla-ansible@stable/2024.1
+    sudo mkdir /etc/kolla
+    sudo chown $USER:$USER /etc/kolla
+    cp $HOME/kolla-openstack/share/kolla-ansible/etc_examples/kolla/* /etc/kolla/
+    cp $HOME/kolla-openstack/share/kolla-ansible/ansible/inventory/all-in-one .
+    cd $HOME/kolla-openstack
+    sudo chown $USER:$USER /etc/kolla
+    kolla-ansible install-deps
+    kolla-genpwd
+    kolla-ansible -i all-in-one bootstrap-servers
+    kolla-ansible -i all-in-one prechecks
+    kolla-ansible -i all-in-one deploy
+
+    pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/2024.1
+    pip install python-neutronclient -c https://releases.openstack.org/constraints/upper/2024.1
+    pip install python-glanceclient -c https://releases.openstack.org/constraints/upper/2024.1
+    pip install python-heatclient -c https://releases.openstack.org/constraints/upper/2024.1
+    pip install python-magnumclient -c https://releases.openstack.org/constraints/upper/2024.1
+
+    kolla-ansible post-deploy
+    cp $SCRIPT_DIR/init-runonce init-runonce
+    ./init-runonce
+
+# Ngrok
+
+
+
 # on a Fedora linux system
 # Creation of fedora coreos image
 STREAM='stable'
@@ -47,17 +91,46 @@ IMAGE=${FILE:0:-6} # remove extension .qcow2
     echo br_netfilter | sudo tee /etc/modules-load.d/kubernetes.conf
 
 
-# Create cluster
+# Create K8s cluster config
     sudo kubeadm init --pod-network-cidr=10.244.0.0/16
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# Magnum CAPI installation
+sudo usermod -aG docker toor
+sudo docker exec -it -u0 magnum_api bash
+    apt update
+    apt install wget
+    pip install magnum-cluster-api
+    wget https://get.helm.sh/helm-v3.13.2-linux-amd64.tar.gz
+    tar -zxvf helm-v3.13.2-linux-amd64.tar.gz
+    mv linux-amd64/helm /usr/local/bin
+
+sudo docker exec -it -u0 magnum_conductor bash
+    apt update
+    apt install wget
+    pip install magnum-cluster-api
+    wget https://get.helm.sh/helm-v3.13.2-linux-amd64.tar.gz
+    tar -zxvf helm-v3.13.2-linux-amd64.tar.gz
+    mv linux-amd64/helm /usr/local/bin
+
+    sudo docker exec -it -u0 magnum_conductor mkdir /var/lib/magnum/.kube
+    sudo docker exec -it -u0 magnum_api mkdir /var/lib/magnum/.kube
+    sudo docker cp ~/.kube/  magnum_api:/var/lib/magnum/
+    sudo docker cp ~/.kube/  magnum_conductor:/var/lib/magnum/
+    sudo docker exec -it -u0 magnum_api chown magnum:magnum /var/lib/magnum/.kube/config
+    sudo docker exec -it -u0 magnum_conductor chown magnum:magnum /var/lib/magnum/.kube/config
+
+    sudo docker restart magnum_api magnum_conductor
+
+
 # Openstack configuration
     source ~/kolla-openstack/bin/activate
     source /etc/kolla/admin-openrc.sh
+    WEB_PASS=$(grep keystone_admin_password /etc/kolla/passwords.yml)
     openstack image create
-        --disk-format=qcow2                                    \
+        --disk-format=qcow2                                     \
         --min-disk=10                                           \
         --min-ram=2                                             \
         --progress                                              \
